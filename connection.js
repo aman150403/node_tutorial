86acbc9fe40c09ed1b8a172d037e1a91c990d717
Aman <amanksingh9098@gmail.com>
2024-11-23 01:32:11 +0530

    mongo update

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const url = process.env.MONGO_URL 
 
 // mongoose.connect(url)
-mongoose.connect(url)
+mongoose.connect(url, {bufferCommands: false})
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch((err) => console.error('Connection error:', err));
 
